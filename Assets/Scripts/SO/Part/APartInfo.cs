c5e16c44355c8b68717b52148f373a3d8ffcd582
Xwilarg <xwilarg@protonmail.com>
2024-08-18 13:35:38 +0200

    Add new kinds of CPU

--- Assets/Scripts/SO/Part/APartInfo.cs
+++ Assets/Scripts/SO/Part/APartInfo.cs
@@ -5,6 +5,8 @@ namespace Gmtk.SO.Part
     public class APartInfo : ScriptableObject
     {
         public string Name;
+        [TextArea]
+        public string Description;
         public GameObject GameObject;
     }
 }
